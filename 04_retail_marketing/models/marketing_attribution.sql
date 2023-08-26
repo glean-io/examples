bd1a335d1c8c8ce189381450c9833997b1697636
Carlos Aguilar <carlos@glean.nyc>
2023-08-26 14:37:11 -0400

    wip

--- 04_retail_marketing/models/marketing_attribution.sql
+++ 04_retail_marketing/models/marketing_attribution.sql
@@ -1,6 +1,7 @@
 select
   oa.attribution_id,
   oa.order_id,
+  o.timestamp as order_timestamp,
   oa.touchpoint_id,
   oa.attribution_share,
   oa.attribution_timestamp,
@@ -9,3 +10,5 @@ select
 from "./data/marketing_attribution.parquet" as oa
   left join "./data/marketing_touchpoints.parquet" as tp
     on tp.touchpoint_id = oa.touchpoint_id
+  left join "./data/dbt/orders.parquet" as o
+    on o.id = oa.order_id
